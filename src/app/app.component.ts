f20e163ec5679a5acd659f08614e9d0d8d70f602
gustavoafo <gustavoafonso.gu@gmail.com>
2018-04-16 18:20:00 -0300

    Commit inicial - pré layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  collapsed = true;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
 }
