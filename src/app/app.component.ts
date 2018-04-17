c2a82d97671fb9ba5509bf54e79c822e414dd160
gustavoafo <gustavoafonso.gu@gmail.com>
2018-04-16 23:31:54 -0300

    Service.ts criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,64 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { AppService } from './app.service';
+import { HttpModule } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'app';
+  dados_busca: any[];
+  dados_especificos: any[];
+  count_busca: number;
 
   collapsed = true;
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
   }
+
+  constructor(private appservice: AppService) {
+
+   // this.buscarTudo();
+   this.teste();
+  // console.log(this.dados_especificos);
+  }
+
+
+
+  iniciarBuscaPorPokemonsEspecificos() {
+    this.dados_busca.forEach(item => {
+      this.buscarPokemonEspecifico(item['url']);
+    });
+  }
+
+
+  teste() {
+    this.appservice.buscaListaCompleta().subscribe(data => {
+     this.appservice.buscarPokemonEspecifico(data['results']['url']).subscribe(sub => {
+       this.dados_especificos.push(sub);
+     })
+    });
+  }
+
+
+   buscarTudo() {
+     this.appservice.buscaListaCompleta().subscribe(data => {
+      this.dados_busca = data['results'];
+      this.count_busca = data['count'];
+    });
+
+    // await this.iniciarBuscaPorPokemonsEspecificos();
+  }
+
+  buscarPokemonEspecifico(url: string) {
+    this.appservice.buscarPokemonEspecifico(url).subscribe(data => {
+      this.dados_especificos.push(data['aqui']);
+    });
+  }
+
 
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class AppService {
+
+    headers: Headers;
+    options: RequestOptions;
+    constructor(public http: Http) {
+
+
+
+        this.headers = new Headers({
+            'Content-Type': 'application/json',
+        });
+        this.options = new RequestOptions({ headers: this.headers });
+    }
+
+
+    buscaListaCompleta() {
+        const url = 'https://pokeapi.co/api/v2/pokemon/';
+        return this.http.get(url, this.options).map(res => res.json()).catch(this.handleError);
+
+    }
+
+    buscarPokemonEspecifico(url: string) {
+        return this.http.get(url, this.options).map(res => res.json()).catch(this.handleError);
+
+    }
+
+    handleError(error) {
+        // console.log(error);
+        return Observable.throw(error.json().error || 'SERVER ERROR');
+    }
+
+
+}
